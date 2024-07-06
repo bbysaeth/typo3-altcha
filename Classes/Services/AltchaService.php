9339da2a5ab20bc922e6cf2c08e006305f09b4e7
Benjamin Bysäth <benjamin@bysaeth.de>
2024-07-06 10:15:05 +0200

    feat: add maximum complexity configuration

--- Classes/Services/AltchaService.php
+++ Classes/Services/AltchaService.php
@@ -6,12 +6,15 @@ namespace BBysaeth\Typo3Altcha\Services;
 
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Resource\Event\GeneratePublicUrlForResourceEvent;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 class AltchaService
 {
     protected array $configuration = [];
-    public function __construct(protected ExtensionConfiguration $extensionConfiguration)
+    protected array $typoscriptSetup = [];
+    public function __construct(protected ExtensionConfiguration $extensionConfiguration, protected ConfigurationManagerInterface $configurationManager)
     {
         $this->init();
     }
@@ -19,13 +22,29 @@ class AltchaService
     protected function init()
     {
         $this->configuration = $this->extensionConfiguration->get('altcha');
+        $this->typoscriptSetup = $this->configurationManager->getConfiguration(
+            ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
+        );
+    }
+
+    protected function getTypoScriptSettings(): array
+    {
+        $typoScriptSetup = $this->configurationManager->getConfiguration(
+            ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
+        );
+        $settings = $typoScriptSetup['plugin.']['tx_altcha.']['settings.'] ?? [];
+        return $settings;
     }
 
     public function createChallenge(string $salt = null, $number = null): array
     {
         $hmac = $this->configuration['hmac'] ?? 'S3cr3t';
+        $typoscriptSettings = $this->getTypoScriptSettings();
+
+        $minComplexity = MathUtility::forceIntegerInRange($typoscriptSettings['minimumComplexity'], 0, 2000000000, 15000);
+        $maxComplexity = MathUtility::forceIntegerInRange($typoscriptSettings['maximumComplexity'], 0, 2000000000, 15000);
         $salt = $salt ?? bin2hex(random_bytes(12));
-        $number = $number ?? random_int(5000, 15000);
+        $number = $number ?? random_int($minComplexity, $maxComplexity);
         $challenge = hash('sha256', $salt . $number);
         $signature = hash_hmac('sha256', $challenge, $hmac);
 
