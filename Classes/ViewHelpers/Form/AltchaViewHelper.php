f2642aaf81fca9e7579d7d53d64b56b89643cd58
Benjamin Bysäth <benjamin@bysaeth.de>
2024-07-13 14:54:10 +0200

    feat: allow hide altcha logo and footer

--- Classes/ViewHelpers/Form/AltchaViewHelper.php
+++ Classes/ViewHelpers/Form/AltchaViewHelper.php
@@ -6,23 +6,34 @@ namespace BBysaeth\Typo3Altcha\ViewHelpers\Form;
 
 use BBysaeth\Typo3Altcha\Services\AltchaService;
 use BBysaeth\Typo3Altcha\Validation\AltchaValidator;
+use Doctrine\DBAL\Configuration;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper;
 
 class AltchaViewHelper extends AbstractFormFieldViewHelper
 {
-    public function __construct(protected AltchaService $altchaService)
+    protected $typoscriptSettings = [];
+
+    public function __construct(protected AltchaService $altchaService, ConfigurationManagerInterface $configurationManager)
     {
+        $typoScriptSetup = $configurationManager->getConfiguration(
+            ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
+        );
+        $this->typoscriptSettings = $typoScriptSetup['plugin.']['tx_altcha.']['settings.'] ?? [];
         parent::__construct();
     }
-
+    
     public function render(): string
     {   
         $name = $this->getName();
         $this->registerFieldNameForFormTokenGeneration($name);
         $challenge = $this->altchaService->createChallenge();
         $container = $this->templateVariableContainer;
+        DebuggerUtility::var_dump($this->typoscriptSettings);
         $container->add('name', $name);
         $container->add('challenge', $challenge);
+        $container->add('customization', $this->typoscriptSettings);
         $content = $this->renderChildren();
         
         return $content;
