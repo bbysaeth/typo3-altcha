cad3735846cea6433b383173e0ee1aaa51409b00
Benjamin Bysäth <benjamin@bysaeth.de>
2024-07-21 22:07:49 +0200

    chore: add icon and update package informations

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,21 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Altcha spam protection for ext:form',
     'description' => 'TYPO3 form element for spam protection by utilizing the proof-of-work mechanism Altcha.',
-    'category' => 'frontend',
+    'category' => 'fe',
     'author' => 'Benjamin Bysäth',
     'author_email' => 'benjamin@bysaeth.de',
     'state' => 'stable',
-    'version' => '0.3.0',
+    'version' => '0.3.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
             'form' => '12.4.0-12.4.99',
+            'extbase' => '12.4.0-12.4.99',
+            'fluid' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            'scheduler' => '12.4.0-12.4.99',
+        ],
     ]
 ];
