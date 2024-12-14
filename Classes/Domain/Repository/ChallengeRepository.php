b64e41cd3c37a893273df01e228bb44f63bebdcb
offlineBlogger <97961093+offlineBlogger@users.noreply.github.com>
2024-12-14 19:55:14 +0100

    [BUGFIX] enable captcha on translated pages

--- Classes/Domain/Repository/ChallengeRepository.php
+++ Classes/Domain/Repository/ChallengeRepository.php
@@ -13,6 +13,7 @@ class ChallengeRepository extends Repository
     {
         $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
         $querySettings->setRespectStoragePage(false);
+        $querySettings->setRespectSysLanguage(false);
         $this->setDefaultQuerySettings($querySettings);
     }
 }
